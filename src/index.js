659672199aa4d59d78746781546e157d35b15f16
magarampage <magarampage@gmail.com>
2018-07-22 22:30:42 +0300

    empty space issues

--- src/index.js
+++ src/index.js
@@ -1,1 +1,91 @@
-export Simple from './Simple';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { Input } from 'sm-react-common-uikit';
+
+
+const defaultComponent = props => <Input {...props} />;
+
+const defaultRenderer = value => value;
+
+export default class EditableTable extends Component {
+	static propTypes = {
+		// array of field descriptors: { placeholder, name, value, component }
+		fields: PropTypes.array.isRequired,
+		// will receive (name, value) as its arguments
+		onChange: PropTypes.func.isRequired
+	};
+
+	constructor() {
+		super();
+		this.state = {
+			activeField: null,
+			fields: []
+		};
+	}
+
+	componentDidMount() {
+		if (this.props.fields.length > 0) {
+			// eslint-disable-next-line react/no-did-mount-set-state
+			this.setState({ fields: this.props.fields })
+		}
+	}
+
+	onChange = (name, value) => {
+		this.props.onChange(name, value);
+	};
+
+	onBlur = () => {
+		this.setState({ activeField: null });
+	};
+
+	setActive = (activeField) => {
+		this.setState({ activeField })
+	}
+
+	isActive = field => field === this.state.activeField;
+
+	handleChange = (name, value, index) => {
+		const newFielsd = [...this.state.fields]
+		newFielsd[index].value = value
+		this.props.onChange(name, value)
+		this.setState({ fields: newFielsd })
+	}
+
+	render() {
+		// eslint-disable-next-line prefer-destructuring
+		const onBlur = this.onBlur;
+		const inputProps = { autoFocus: 'autofocus', onBlur };
+		return (
+			<table className="table table-striped table-bordered detail-view sf-editable-table">
+				<tbody>
+					{this.state.fields.map(({
+						name, title, value, constant, component, renderer
+					}, index) => {
+						// eslint-disable-next-line no-param-reassign
+						component = component || defaultComponent;
+						// eslint-disable-next-line no-param-reassign
+						renderer = renderer || defaultRenderer;
+
+						return (
+							<tr key={name}>
+								<th width="30%">{title}</th>
+								<td
+									onClick={() => this.setActive(name)}
+									width="70%"
+								>
+									{this.isActive(name) && !constant ?
+										component({
+											value,
+											inputProps,
+											// eslint-disable-next-line no-shadow
+											onChange: (value) => {
+												this.handleChange(name, value, index)
+											}
+										}) : renderer(value)}
+								</td>
+							</tr>)
+					})}
+				</tbody>
+			</table>)
+	}
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,60 @@
-// import * as exports from './index';
-// console.log(exports);
-// import Simple from '../src/Simple';
-//
-// // describe('expots', () => {
-// // 	// it('should exports components', () => {
-// // 	// 	expect(exports).toEqual(expect.objectContaining({
-// // 	// 		Simple
-// // 	// 	}))
-// // 	// });
-// // 	it('should sd', () => {
-// // 		expect(1).toEqual(2)
-// // 	})
-// // });
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+import EditableTable from './index'
+
+configure({ adapter: new Adapter() })
+const spy = jest.fn()
+
+function setup(customProps, lifeCycle = false) {
+	const props = {
+		onChange: spy,
+		fields: [{
+			name: 'name',
+			title: 'title',
+			value: 'value',
+			constant: true
+		}],
+		...customProps
+	}
+	EditableTable.prototype.setActive = jest.fn()
+	const container = shallow(<EditableTable {...props} />, { disableLifecycleMethods: lifeCycle })
+	return { container, props }
+}
+
+describe('EditableTable component', () => {
+
+	it('should render the component', () => {
+		const { container } = setup()
+		expect(container.exists()).toBe(true)
+	})
+
+	it('should have specified first tag and class', () => {
+		const { container } = setup()
+		expect(container.first().type()).toEqual('table')
+		expect(container.first().props().className).toEqual('table table-striped table-bordered detail-view sf-editable-table')
+	})
+	it('should have tbody tag inside table', () => {
+		const { container } = setup()
+		console.log(container.debug())
+		expect(container.find('table tbody').exists()).toEqual(true)
+	})
+
+	it('should have specified table structure', () => {
+		const { container } = setup()
+		expect(container.find('table tbody tr').first().exists()).toEqual(true)
+		expect(container.find('table tbody tr').first().find('th').exists()).toEqual(true)
+		expect(container.find('table tbody tr').first().find('th').text()).toEqual('title')
+
+		const instance = container.instance()
+		jest.spyOn(instance, 'setActive')
+		container.find('table tbody tr').first().find('td').simulate('click')
+		expect(instance.setActive).toHaveBeenCalled()
+	})
+
+	it('should call setstate inside cDm', () => {
+		const { container, props } = setup()
+		expect(container.state('fields')).toEqual(props.fields)
+	})
+})
+
